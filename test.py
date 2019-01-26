2cf4a0c6a862641e0a36edc7f3c9e3766a853a2c
hai <lengochai97@gmail.com>
2019-01-26 18:14:03 +0700

    Print more log details

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pickle
 
+import numpy as np
 from sklearn import svm
 
 import utils
@@ -8,7 +9,8 @@ from constants import DIR_MODEL, DIR_TEST, LABELS
 
 
 def test():
-    X_test, y_test = utils.read_process_images(DIR_TEST, LABELS)
+    X_test, y_test, paths = utils.read_process_images(
+        DIR_TEST, LABELS, return_paths=True)
     print(f'X_test shape: {X_test.shape}')
     print(f'y_test shape: {y_test.shape}')
 
@@ -18,14 +20,26 @@ def test():
     # load model from disk
     loaded_model = pickle.load(open(DIR_MODEL, 'rb'))
 
+    # predict at test data
+    y_pred = loaded_model.predict(X_test_fc6)
+
     print('Y test = ')
     print(y_test)
     print('Y predict = ')
-    # predict at test data
-    print(loaded_model.predict(X_test_fc6))
+    print(y_pred)
 
     scores = loaded_model.score(X_test_fc6, y_test)
     print("accuracy = %f" % scores)
+
+    # Print false predictions
+    diff = y_test != y_pred
+    if np.any(diff):
+        print('false predictions:')
+        print(paths[diff])
+
+    # Print confidence score of each sample
+    print('confidence scores:')
+    print(loaded_model.decision_function(X_test_fc6))
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 
-def read_process_images(dir, labels):
+def read_process_images(dir, labels, return_paths=False):
     """Prepares data to feed VGG16 model.
 
     Uses labels as the name of the directories containing the images.
@@ -14,17 +14,21 @@ def read_process_images(dir, labels):
     Args:
         dir: where to read images from
         labels: list of image labels
+        return_paths: whether to return file paths
 
     Returns:
-        X, y: converted dataset    
+        X, y: converted dataset
+        paths: image file paths if return_paths is True
     """
 
     X = []
     y = []
+    paths = []
 
     for i, label in enumerate(labels):
         for fn in os.listdir(os.path.join(dir, label)):
-            img = Image.open(os.path.join(dir, label, fn))
+            fp = os.path.join(dir, label, fn)
+            img = Image.open(fp)
 
             # Ensure 224x224x3
             assert len(img.getbands()) == 3
@@ -32,5 +36,9 @@ def read_process_images(dir, labels):
 
             X.append(np.array(img))
             y.append(i)
+            paths.append(fp)
+
+    if return_paths:
+        return np.array(X), np.array(y), np.array(paths)
 
     return np.array(X), np.array(y)
