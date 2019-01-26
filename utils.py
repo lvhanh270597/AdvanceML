16025bd2a5e215fbf661fab0d4fc73823fca12e1
hai <lengochai97@gmail.com>
2019-01-26 16:40:48 +0700

    Add docstring

--- utils.py
+++ utils.py
@@ -5,6 +5,20 @@ from PIL import Image
 
 
 def read_process_images(dir, labels):
+    """Prepares data to feed VGG16 model.
+
+    Uses labels as the name of the directories containing the images.
+    Converts each of the images to have the shape of 224x224x3, which is
+    required by the (default) VGG16 model.
+
+    Args:
+        dir: where to read images from
+        labels: list of image labels
+
+    Returns:
+        X, y: converted dataset    
+    """
+
     X = []
     y = []
 
